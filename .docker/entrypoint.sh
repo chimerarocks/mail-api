7becb8e4ba8b77ca8851aae306b1c042131b0067
joaopedro <joaopedrodslv@gmail.com>
2020-11-05 23:22:09 -0300

    test environment setup

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -4,9 +4,14 @@ if [ ! -e .env ]
  then cp .env.example .env
 fi
 
+if [ ! -e .env.testing ]
+ then cp .env.testing.example .env.testing
+fi
+
 chown -R www-data:www-data .
 composer install
 php artisan key:generate
 php artisan migrate
 
 php-fpm
+.
