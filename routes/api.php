1a695a28978d0c9353ef0a561cf7ec8f7ba4790a
joaopedro <joaopedrodslv@gmail.com>
2020-11-08 16:24:51 -0300

    add post mail route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\V1\Controllers\MailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::name('v1.')->prefix('v1')->group(function() {
+    Route::post('/mail', [MailController::class, 'send'])->name('mail');
 });
