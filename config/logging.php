035e4aceb9f95d6672437544c7eed465ff3fbf90
joaopedro <joaopedrodslv@gmail.com>
2020-11-11 04:39:56 -0300

    add mailing log

--- config/logging.php
+++ config/logging.php
@@ -54,6 +54,13 @@ return [
             'days' => 14,
         ],
 
+        'daily_mailing' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/daily_mailing.log'),
+            'level' => env('LOG_LEVEL', 'info'),
+            'days' => 14,
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
